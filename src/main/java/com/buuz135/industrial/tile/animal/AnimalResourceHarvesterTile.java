b117fcb06474fc3bd369b90921b2fb9222f6c9ef
Buuz135 <buuz135@hotmail.com>
2017-05-02 00:03:32 +0200

    ADDED:   Animal Resource Harvester

--- src/main/java/com/buuz135/industrial/tile/animal/AnimalResourceHarvesterTile.java
+++ src/main/java/com/buuz135/industrial/tile/animal/AnimalResourceHarvesterTile.java
@@ -1,19 +1,34 @@
 package com.buuz135.industrial.tile.animal;
 
+import com.buuz135.industrial.proxy.FluidsRegistry;
 import com.buuz135.industrial.tile.WorkingAreaElectricMachine;
+import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
+import net.ndrei.teslacorelib.containers.BasicTeslaContainer;
+import net.ndrei.teslacorelib.containers.FilteredSlot;
+import net.ndrei.teslacorelib.gui.BasicTeslaGuiContainer;
+import net.ndrei.teslacorelib.gui.IGuiContainerPiece;
+import net.ndrei.teslacorelib.gui.TiledRenderedGuiPiece;
+import net.ndrei.teslacorelib.inventory.BoundingRectangle;
+import net.ndrei.teslacorelib.inventory.ColoredItemHandler;
 
 import java.util.List;
 
 public class AnimalResourceHarvesterTile extends WorkingAreaElectricMachine {
 
     private ItemStackHandler outItems;
+    private IFluidTank milkTank;
 
     public AnimalResourceHarvesterTile() {
         super(AnimalResourceHarvesterTile.class.getName().hashCode());
@@ -22,45 +37,46 @@ public class AnimalResourceHarvesterTile extends WorkingAreaElectricMachine {
     @Override
     protected void initializeInventories() {
         super.initializeInventories();
-        outItems = new ItemStackHandler(3 * 6);
-//        this.addInventory(new ColoredItemHandler(outItems, EnumDyeColor.GREEN, "Fish output", new BoundingRectangle(18 * 3, 25, 18 * 6, 18 * 3)) {
-//            @Override
-//            public boolean canInsertItem(int slot, ItemStack stack) {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean canExtractItem(int slot) {
-//                return true;
-//            }
-//
-//            @Override
-//            public List<Slot> getSlots(BasicTeslaContainer container) {
-//                List<Slot> slots = super.getSlots(container);
-//                BoundingRectangle box = this.getBoundingBox();
-//                int i = 0;
-//                for (int y = 0; y < 3; y++) {
-//                    for (int x = 0; x < 6; x++) {
-//                        slots.add(new FilteredSlot(this.getItemHandlerForContainer(), i, box.getLeft() + 1 + x * 18, box.getTop() + 1 + y * 18));
-//                        ++i;
-//                    }
-//                }
-//                return slots;
-//            }
-//
-//            @Override
-//            public List<IGuiContainerPiece> getGuiContainerPieces(BasicTeslaGuiContainer container) {
-//                List<IGuiContainerPiece> pieces = super.getGuiContainerPieces(container);
-//
-//                BoundingRectangle box = this.getBoundingBox();
-//                pieces.add(new TiledRenderedGuiPiece(box.getLeft(), box.getTop(), 18, 18,
-//                        6, 3,
-//                        BasicTeslaGuiContainer.MACHINE_BACKGROUND, 108, 225, EnumDyeColor.GREEN));
-//
-//                return pieces;
-//            }
-//        });
-//        this.addInventoryToStorage(outFish, "water_resource_collector_out");
+        milkTank = this.addFluidTank(FluidsRegistry.MILK,8000, EnumDyeColor.WHITE,"Milk tank",new BoundingRectangle(50, 25, 18, 54));
+        outItems = new ItemStackHandler(3 * 4);
+        this.addInventory(new ColoredItemHandler(outItems, EnumDyeColor.ORANGE, "Fish output", new BoundingRectangle(18 * 5+3, 25, 18 * 6, 18 * 3)) {
+            @Override
+            public boolean canInsertItem(int slot, ItemStack stack) {
+                return false;
+            }
+
+            @Override
+            public boolean canExtractItem(int slot) {
+                return true;
+            }
+
+            @Override
+            public List<Slot> getSlots(BasicTeslaContainer container) {
+                List<Slot> slots = super.getSlots(container);
+                BoundingRectangle box = this.getBoundingBox();
+                int i = 0;
+                for (int y = 0; y < 3; y++) {
+                    for (int x = 0; x < 4; x++) {
+                        slots.add(new FilteredSlot(this.getItemHandlerForContainer(), i, box.getLeft() + 1 + x * 18, box.getTop() + 1 + y * 18));
+                        ++i;
+                    }
+                }
+                return slots;
+            }
+
+            @Override
+            public List<IGuiContainerPiece> getGuiContainerPieces(BasicTeslaGuiContainer container) {
+                List<IGuiContainerPiece> pieces = super.getGuiContainerPieces(container);
+
+                BoundingRectangle box = this.getBoundingBox();
+                pieces.add(new TiledRenderedGuiPiece(box.getLeft(), box.getTop(), 18, 18,
+                        4, 3,
+                        BasicTeslaGuiContainer.MACHINE_BACKGROUND, 108, 225, EnumDyeColor.ORANGE));
+
+                return pieces;
+            }
+        });
+        this.addInventoryToStorage(outItems, "animal_resource_harvester_out");
     }
 
     @Override
@@ -78,8 +94,14 @@ public class AnimalResourceHarvesterTile extends WorkingAreaElectricMachine {
         for (EntitySheep sheep : animals) {
             if (!sheep.getSheared()) {
                 List<ItemStack> stacks = sheep.onSheared(new ItemStack(Items.SHEARS), this.world, null, 0);
+                for (ItemStack stack : stacks){
+                    ItemHandlerHelper.insertItem(outItems,stack,false);
+                }
+                return 1;
             }
         }
+        List<EntityCow> cows = this.world.getEntitiesWithinAABB(EntityCow.class, getWorkingArea());
+        milkTank.fill(new FluidStack(FluidsRegistry.MILK,cows.size()*1000),true);
         return 1;
     }
 }
