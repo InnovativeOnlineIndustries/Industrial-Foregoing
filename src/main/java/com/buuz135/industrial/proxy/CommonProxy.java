5c9d0a8ed5baacc651fb095e79ea8ffa786a6ce6
Buuz135 <buuz135@hotmail.com>
2017-06-06 18:09:11 +0200

    Removed the Bucket Latex Recipe

--- src/main/java/com/buuz135/industrial/proxy/CommonProxy.java
+++ src/main/java/com/buuz135/industrial/proxy/CommonProxy.java
@@ -1,14 +1,10 @@
 package com.buuz135.industrial.proxy;
 
 import com.buuz135.industrial.config.CustomConfiguration;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
-import net.minecraftforge.common.ForgeModContainer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fluids.UniversalBucket;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import java.util.Random;
 
@@ -24,7 +20,6 @@ public class CommonProxy {
         ItemRegistry.registerItems();
         BlockRegistry.registerBlocks();
 
-        GameRegistry.addSmelting(UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, FluidsRegistry.LATEX), new ItemStack(ItemRegistry.plastic), 0);
         MinecraftForge.EVENT_BUS.register(new MeatFeederTickHandler());
         MinecraftForge.EVENT_BUS.register(new MobDeathHandler());
 
