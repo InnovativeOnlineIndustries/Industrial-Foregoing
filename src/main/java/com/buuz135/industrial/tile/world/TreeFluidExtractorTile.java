7fe8a5cd14fa7716c7bbb462362f39fcfbb9e8db
Buuz135 <buuz135@hotmail.com>
2017-07-07 10:20:07 +0200

    % Fixed Tree Fluid Extractor not working when the world time didn't change

--- src/main/java/com/buuz135/industrial/tile/world/TreeFluidExtractorTile.java
+++ src/main/java/com/buuz135/industrial/tile/world/TreeFluidExtractorTile.java
@@ -17,6 +17,7 @@ import net.ndrei.teslacorelib.tileentities.SidedTileEntity;
 public class TreeFluidExtractorTile extends SidedTileEntity {
 
     private IFluidTank tank;
+    private int tick;
 
     public TreeFluidExtractorTile() {
         super(TreeFluidExtractorTile.class.getName().hashCode());
@@ -26,6 +27,7 @@ public class TreeFluidExtractorTile extends SidedTileEntity {
     protected void initializeInventories() {
         super.initializeInventories();
         tank = this.addFluidTank(FluidsRegistry.LATEX, 8000, EnumDyeColor.GRAY, "Latex tank", new BoundingRectangle(17, 25, 18, 54));
+        tick = 0;
     }
 
     @Override
@@ -37,8 +39,11 @@ public class TreeFluidExtractorTile extends SidedTileEntity {
     protected void innerUpdate() {
         if (((CustomOrientedBlock) this.getBlockType()).isWorkDisabled()) return;
         if (this.getWorld().isRemote) return;
-        if (this.world.getWorldTime() % 5 == 0 && BlockUtils.isLog(this.world, this.pos.offset(this.getFacing().getOpposite())))
+        if (tick == 5 && BlockUtils.isLog(this.world, this.pos.offset(this.getFacing().getOpposite()))) {
             tank.fill(new FluidStack(FluidsRegistry.LATEX, 1), true);
+            tick = 0;
+        }
+        ++tick;
     }
 
 
