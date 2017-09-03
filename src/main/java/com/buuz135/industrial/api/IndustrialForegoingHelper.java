c8ae9f1696135d1a1cbbb44f35422e2be4f9f95a
Buuz135 <buuz135@hotmail.com>
2017-09-03 14:11:01 +0200

    + Added some docs

--- src/main/java/com/buuz135/industrial/api/IndustrialForegoingHelper.java
+++ src/main/java/com/buuz135/industrial/api/IndustrialForegoingHelper.java
@@ -12,9 +12,15 @@ import java.util.stream.Collectors;
 public class IndustrialForegoingHelper {
 
     public static final String MOD_ID = "industrialforegoing";
-    public static final String API_VERSION = "1";
+    public static final String API_VERSION = "2";
     public static final String API_ID = MOD_ID + "api";
 
+    /**
+     * Adds a new BioReactor entry that wasn't already there.
+     *
+     * @param entry The BioReactor entry to add.
+     * @return true if it is added, false if don't.
+     */
     public static boolean addBioReactorEntry(BioReactorEntry entry) {
         if (BioReactorEntry.BIO_REACTOR_ENTRIES.stream().noneMatch(entry1 -> entry.doesStackMatch(entry1.getStack()))) {
             BioReactorEntry.BIO_REACTOR_ENTRIES.add(entry);
@@ -23,6 +29,12 @@ public class IndustrialForegoingHelper {
         return false;
     }
 
+    /**
+     * Removes a BioReactor entry.
+     *
+     * @param stack The ItemStack of a BioReactor entry to remove.
+     * @return true if it is removed, false if don't.
+     */
     public static boolean removeBioReactorEntry(ItemStack stack) {
         if (BioReactorEntry.BIO_REACTOR_ENTRIES.stream().anyMatch(entry -> entry.doesStackMatch(stack))) {
             BioReactorEntry.BIO_REACTOR_ENTRIES = BioReactorEntry.BIO_REACTOR_ENTRIES.stream().filter(entry -> !entry.doesStackMatch(stack)).collect(Collectors.toList());
@@ -31,6 +43,11 @@ public class IndustrialForegoingHelper {
         return false;
     }
 
+    /** Adds a new LaserEntry entry that wasn't already there.
+     *
+     * @param entry The LaserEntry entry to add.
+     * @return true if it is added, false if don't.
+     */
     public static boolean addLaserDrillEntry(LaserDrillEntry entry) {
         if (LaserDrillEntry.LASER_DRILL_ENTRIES.stream().noneMatch(entry1 -> entry1.getStack().isItemEqual(entry.getStack()))) {
             LaserDrillEntry.LASER_DRILL_ENTRIES.add(entry);
@@ -39,6 +56,11 @@ public class IndustrialForegoingHelper {
         return false;
     }
 
+    /** Removes a LaserDrill entry.
+     *
+     * @param stack The ItemStack of a LaserDrill entry to remove.
+     * @return true if it is removed, false if don't.
+     */
     public static boolean removeLaserDrillEntry(ItemStack stack) {
         if (LaserDrillEntry.LASER_DRILL_ENTRIES.stream().anyMatch(entry -> stack.isItemEqual(entry.getStack()))) {
             LaserDrillEntry.LASER_DRILL_ENTRIES = LaserDrillEntry.LASER_DRILL_ENTRIES.stream().filter(entry -> !entry.getStack().isItemEqual(stack)).collect(Collectors.toList());
@@ -46,6 +68,11 @@ public class IndustrialForegoingHelper {
         return false;
     }
 
+    /** Adds a new SludgeRefiner entry that wasn't already there.
+     *
+     * @param entry The SludgeRefiner entry to add.
+     * @return true if it is added, false if don't.
+     */
     public static boolean addSludgeRefinerEntry(SludgeEntry entry) {
         if (SludgeEntry.SLUDGE_RECIPES.stream().noneMatch(entry1 -> entry1.getStack().isItemEqual(entry.getStack()))) {
             SludgeEntry.SLUDGE_RECIPES.add(entry);
@@ -54,6 +81,11 @@ public class IndustrialForegoingHelper {
         return false;
     }
 
+    /** Removes a SludgeRefiner entry.
+     *
+     * @param stack The ItemStack of a SludgeRefiner entry to remove.
+     * @return true if it is removed, false if don't.
+     */
     public static boolean removeSludgeRefinerEntry(ItemStack stack) {
         if (SludgeEntry.SLUDGE_RECIPES.stream().anyMatch(entry -> entry.getStack().isItemEqual(stack))) {
             SludgeEntry.SLUDGE_RECIPES = SludgeEntry.SLUDGE_RECIPES.stream().filter(entry -> !entry.getStack().isItemEqual(stack)).collect(Collectors.toList());
@@ -62,6 +94,10 @@ public class IndustrialForegoingHelper {
         return false;
     }
 
+    /** Registers a new IPlantRecollectable at the start of the list giving it preference over the others that are already there.
+     *
+     * @param recollectable The IPlantRecollectable to register
+     */
     public static void addPlantRecollectable(IPlantRecollectable recollectable) {
         IPlantRecollectable.PLANT_RECOLLECTABLES.add(0, recollectable);
     }
