55a1a6bcaa29dde1037526e727597582fd0e7b4a
Buuz135 <buuz135@hotmail.com>
2017-09-26 19:48:22 +0200

    % Fixed Villager Trade Exchanger not syncing to server. Closes #55

--- src/main/java/com/buuz135/industrial/tile/mob/VillagerTradeExchangerTile.java
+++ src/main/java/com/buuz135/industrial/tile/mob/VillagerTradeExchangerTile.java
@@ -16,11 +16,14 @@ import net.minecraft.village.MerchantRecipe;
 import net.minecraft.village.MerchantRecipeList;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemHandlerHelper;
+import net.ndrei.teslacorelib.TeslaCoreLib;
 import net.ndrei.teslacorelib.gui.BasicRenderedGuiPiece;
 import net.ndrei.teslacorelib.gui.BasicTeslaGuiContainer;
 import net.ndrei.teslacorelib.gui.IGuiContainerPiece;
 import net.ndrei.teslacorelib.inventory.BoundingRectangle;
+import net.ndrei.teslacorelib.netsync.SimpleNBTMessage;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -102,21 +105,46 @@ public class VillagerTradeExchangerTile extends CustomElectricMachine {
         pieces.add(new ArrowInfoPiece(138, 28, 17, 56, "Select previous trade") {
             @Override
             protected void clicked() {
-                if (merchantRecipes == null || merchantRecipes.size() == 0) return;
-                current = (current - 1);
-                if (current < 0) current = merchantRecipes.size() - 1;
-
+                if (TeslaCoreLib.INSTANCE.isClientSide()) {
+                    VillagerTradeExchangerTile.this.sendToServer(VillagerTradeExchangerTile.this.setupSpecialNBTMessage("TRADE_PREV"));
+                }
             }
         });
         pieces.add(new ArrowInfoPiece(156, 28, 33, 56, "Select next trade") {
             @Override
             protected void clicked() {
-                if (merchantRecipes == null || merchantRecipes.size() == 0) return;
-                current = (current + 1) % merchantRecipes.size();
+                if (TeslaCoreLib.INSTANCE.isClientSide()) {
+                    VillagerTradeExchangerTile.this.sendToServer(VillagerTradeExchangerTile.this.setupSpecialNBTMessage("TRADE_NEXT"));
+                }
             }
         });
         return pieces;
     }
 
 
+    @Nullable
+    @Override
+    protected SimpleNBTMessage processClientMessage(String messageType, NBTTagCompound compound) {
+        super.processClientMessage(messageType, compound);
+        if (messageType.equals("TRADE_NEXT")) {
+            nextTrade();
+        }
+        if (messageType.equals("TRADE_PREV")) {
+            prevTrade();
+        }
+        return null;
+    }
+
+    public void nextTrade() {
+        if (merchantRecipes == null || merchantRecipes.size() == 0) return;
+        current = (current + 1) % merchantRecipes.size();
+        forceSync();
+    }
+
+    public void prevTrade() {
+        if (merchantRecipes == null || merchantRecipes.size() == 0) return;
+        current = (current - 1);
+        if (current < 0) current = merchantRecipes.size() - 1;
+        forceSync();
+    }
 }
