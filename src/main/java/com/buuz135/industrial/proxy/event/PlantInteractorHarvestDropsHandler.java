74c3a2840e0022ed317b1980ca53a08c6c34be7b
Buuz135 <buuz135@hotmail.com>
2018-04-27 15:13:12 +0200

    Fixed plant interactor giving Mystical agriculture seeds when its not supposed to

--- src/main/java/com/buuz135/industrial/proxy/event/PlantInteractorHarvestDropsHandler.java
+++ src/main/java/com/buuz135/industrial/proxy/event/PlantInteractorHarvestDropsHandler.java
@@ -3,11 +3,12 @@ package com.buuz135.industrial.proxy.event;
 import com.buuz135.industrial.tile.agriculture.PlantInteractorTile;
 import com.buuz135.industrial.utils.BlockUtils;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class PlantInteractorHarvestDropsHandler {
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onHarvest(BlockEvent.HarvestDropsEvent event) {
         for (PlantInteractorTile tile : PlantInteractorTile.WORKING_TILES) {
             if (tile.getWorld().equals(event.getWorld()) && BlockUtils.getBlockPosInAABB(tile.getWorkingArea()).contains(event.getPos())) {
