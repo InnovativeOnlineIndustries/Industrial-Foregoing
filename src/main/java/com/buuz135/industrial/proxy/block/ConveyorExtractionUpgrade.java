5dbf4a051fd83f67026ff14569f18ba94ce1823d
TheCodedOne <dr.kevincodex@gmail.com>
2018-05-25 16:50:08 +0100

    Implement fast extraction upgrade

--- src/main/java/com/buuz135/industrial/proxy/block/ConveyorExtractionUpgrade.java
+++ src/main/java/com/buuz135/industrial/proxy/block/ConveyorExtractionUpgrade.java
@@ -3,17 +3,25 @@ package com.buuz135.industrial.proxy.block;
 import com.buuz135.industrial.api.conveyor.ConveyorUpgrade;
 import com.buuz135.industrial.api.conveyor.ConveyorUpgradeFactory;
 import com.buuz135.industrial.api.conveyor.IConveyorContainer;
+import com.buuz135.industrial.proxy.ItemRegistry;
+import com.buuz135.industrial.registry.IFRegistries;
 import com.buuz135.industrial.utils.Reference;
+import com.google.common.collect.Sets;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
 import javax.annotation.Nonnull;
+import java.util.Collection;
 
 public class ConveyorExtractionUpgrade extends ConveyorUpgrade {
 
@@ -21,10 +29,12 @@ public class ConveyorExtractionUpgrade extends ConveyorUpgrade {
         super(container, factory, side);
     }
 
-    public static Cuboid NORTHBB = new Cuboid(0.0625 * 4, 0.0625*3, -0.0625 * 2, 0.0625 * 12, 0.0625 * 11, 0.0625 * 3,EnumFacing.NORTH.getIndex());
-    public static Cuboid SOUTHBB = new Cuboid(0.0625 * 4, 0.0625*3, 0.0625*13, 0.0625 * 12, 0.0625 * 11, 0.0625*18,EnumFacing.SOUTH.getIndex());
-    public static Cuboid EASTBB = new Cuboid(0.0625*13, 0.0625*3, 0.0625 * 4, 0.0625*18, 0.0625 * 11, 0.0625 * 12,EnumFacing.EAST.getIndex());
-    public static Cuboid WESTBB = new Cuboid(-0.0625 * 2, 0.0625*3, 0.0625 * 4, 0.0625 * 3, 0.0625 * 11, 0.0625 * 12,EnumFacing.WEST.getIndex());
+    private boolean fast = false;
+
+    public static Cuboid NORTHBB = new Cuboid(0.0625 * 4, 0.0625 * 3, -0.0625 * 2, 0.0625 * 12, 0.0625 * 11, 0.0625 * 3, EnumFacing.NORTH.getIndex());
+    public static Cuboid SOUTHBB = new Cuboid(0.0625 * 4, 0.0625 * 3, 0.0625 * 13, 0.0625 * 12, 0.0625 * 11, 0.0625 * 18, EnumFacing.SOUTH.getIndex());
+    public static Cuboid EASTBB = new Cuboid(0.0625 * 13, 0.0625 * 3, 0.0625 * 4, 0.0625 * 18, 0.0625 * 11, 0.0625 * 12, EnumFacing.EAST.getIndex());
+    public static Cuboid WESTBB = new Cuboid(-0.0625 * 2, 0.0625 * 3, 0.0625 * 4, 0.0625 * 3, 0.0625 * 11, 0.0625 * 12, EnumFacing.WEST.getIndex());
 
     @Override
     public Cuboid getBoundingBox() {
@@ -41,11 +51,45 @@ public class ConveyorExtractionUpgrade extends ConveyorUpgrade {
         }
     }
 
+
+    @Override
+    public NBTTagCompound serializeNBT() {
+        NBTTagCompound compound = new NBTTagCompound();
+        compound.setBoolean("fast", fast);
+        return compound;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound nbt) {
+        fast = nbt.getBoolean("fast");
+    }
+
+    @Override
+    public boolean onUpgradeActivated(EntityPlayer player, EnumHand hand) {
+        ItemStack held = player.getHeldItem(hand);
+        if (held.getItem() == Items.GLOWSTONE_DUST && !fast) {
+            fast = true;
+            held.shrink(1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Collection<ItemStack> getDrops() {
+        if (!fast)
+            return super.getDrops();
+        return Sets.newHashSet(
+                new ItemStack(ItemRegistry.conveyorUpgradeItem, 1, IFRegistries.CONVEYOR_UPGRADE_REGISTRY.getID(getFactory()) - 1),
+                new ItemStack(Items.GLOWSTONE_DUST)
+        );
+    }
+
     @Override
     public void update() {
-        if(getWorld().isRemote)
+        if (getWorld().isRemote)
             return;
-        if (getWorld().getTotalWorldTime() % 10 == 0) {
+        if (getWorld().getTotalWorldTime() % (fast ? 10 : 20) == 0) {
             BlockPos offsetPos = getPos().offset(getSide());
             TileEntity tile = getWorld().getTileEntity(offsetPos);
             if (tile != null) {
@@ -83,13 +127,13 @@ public class ConveyorExtractionUpgrade extends ConveyorUpgrade {
         @Override
         @Nonnull
         public ResourceLocation getModel(EnumFacing upgradeSide, EnumFacing conveyorFacing) {
-            return new ResourceLocation(Reference.MOD_ID, "block/conveyor_upgrade_extractor_"+upgradeSide.getName().toLowerCase());
+            return new ResourceLocation(Reference.MOD_ID, "block/conveyor_upgrade_extractor_" + upgradeSide.getName().toLowerCase());
         }
 
         @Nonnull
         @Override
         public ResourceLocation getItemModel() {
-            return new ResourceLocation(Reference.MOD_ID,"conveyor_extraction_upgrade");
+            return new ResourceLocation(Reference.MOD_ID, "conveyor_extraction_upgrade");
         }
     }
 }
