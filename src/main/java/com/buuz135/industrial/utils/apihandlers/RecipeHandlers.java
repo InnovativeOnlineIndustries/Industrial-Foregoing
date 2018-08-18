6127d5da68156338eb58214f7dda853eea24c8ae
Buuz135 <buuz135@hotmail.com>
2018-08-18 18:48:28 +0200

    Added glowstone to the laser drill

--- src/main/java/com/buuz135/industrial/utils/apihandlers/RecipeHandlers.java
+++ src/main/java/com/buuz135/industrial/utils/apihandlers/RecipeHandlers.java
@@ -128,6 +128,7 @@ public class RecipeHandlers {
         checkAndAddLaserDrill(11, "oreCobalt", 2);
         checkAndAddLaserDrill(3, "oreOsmium", 4);
         checkAndAddLaserDrill(4, "oreArdite", 1);
+        checkAndAddLaserDrill(4, "glowstone", 2);
     }
 
     public static void loadSludgeRefinerEntries() {
