d3874104660b0b9d971330016e2d1bd299e45234
Buuz135 <buuz135@hotmail.com>
2018-11-11 17:19:30 +0100

    Fixed small color checking inefficiency, closes #450

--- src/main/java/com/buuz135/industrial/utils/ColorUtils.java
+++ src/main/java/com/buuz135/industrial/utils/ColorUtils.java
@@ -44,15 +44,13 @@ public class ColorUtils {
         if (sprite.getFrameCount() == 0) return -1;
         int[][] pixelMatrix = sprite.getFrameTextureData(0);
         int total = 0, red = 0, blue = 0, green = 0;
-        for (int[] pixelArray : pixelMatrix) {
-            for (int pixel : pixelArray) {
-                Color color = new Color(pixel);
-                if (color.getAlpha() < 255) continue;
-                ++total;
-                red += color.getRed();
-                green += color.getGreen();
-                blue += color.getBlue();
-            }
+        for (int pixel : pixelMatrix[pixelMatrix.length - 1]) {
+            Color color = new Color(pixel);
+            if (color.getAlpha() < 255) continue;
+            ++total;
+            red += color.getRed();
+            green += color.getGreen();
+            blue += color.getBlue();
         }
         if (total > 0) return new Color(red / total, green / total, blue / total, 255).getRGB();
         return -1;
@@ -63,16 +61,14 @@ public class ColorUtils {
         if (sprite.getFrameCount() == 0) return -1;
         int[][] pixelMatrix = sprite.getFrameTextureData(0);
         int total = 0, red = 0, blue = 0, green = 0;
-        for (int[] pixelArray : pixelMatrix) {
-            for (int pixel : pixelArray) {
-                Color color = new Color(pixel);
-                if (color.getAlpha() < 255 || (color.getRGB() - filter.getRGB() < 100 && color.getRGB() - filter.getRGB() > 100))
-                    continue;
-                ++total;
-                red += color.getRed();
-                green += color.getGreen();
-                blue += color.getBlue();
-            }
+        for (int pixel : pixelMatrix[pixelMatrix.length - 1]) {
+            Color color = new Color(pixel);
+            if (color.getAlpha() < 255 || (color.getRGB() - filter.getRGB() < 100 && color.getRGB() - filter.getRGB() > 100))
+                continue;
+            ++total;
+            red += color.getRed();
+            green += color.getGreen();
+            blue += color.getBlue();
         }
         if (total > 0) return new Color(red / total, green / total, blue / total, 255).brighter().getRGB();
         return -1;
