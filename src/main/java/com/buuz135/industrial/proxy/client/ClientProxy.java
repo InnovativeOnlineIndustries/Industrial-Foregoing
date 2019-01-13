361924f04fd4704182168a8bfab09b8da34f74e6
Coded <coded@hrznstudio.com>
2019-01-13 17:00:44 +0000

    its 2019

--- src/main/java/com/buuz135/industrial/proxy/client/ClientProxy.java
+++ src/main/java/com/buuz135/industrial/proxy/client/ClientProxy.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Industrial Foregoing.
  *
- * Copyright 2018, Buuz135
+ * Copyright 2019, Buuz135
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in the
