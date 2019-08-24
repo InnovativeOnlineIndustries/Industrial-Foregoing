0122e26cf349f3e08247e7b0cb40e4c7efc8509e
Buuz135 <buuz135@hotmail.com>
2019-08-24 19:13:06 +0200

    Unfinished mob essence tool item

--- src/main/java/com/buuz135/industrial/module/ModuleTool.java
+++ src/main/java/com/buuz135/industrial/module/ModuleTool.java
@@ -1,6 +1,7 @@
 package com.buuz135.industrial.module;
 
 import com.buuz135.industrial.item.MeatFeederItem;
+import com.buuz135.industrial.item.MobEssenceToolItem;
 import com.buuz135.industrial.item.MobImprisonmentToolItem;
 import com.buuz135.industrial.item.infinity.ItemInfinityDrill;
 import com.buuz135.industrial.utils.Reference;
@@ -19,6 +20,7 @@ public class ModuleTool implements IModule {
     public static MeatFeederItem MEAT_FEEDER;
     public static MobImprisonmentToolItem MOB_IMPRISONMENT_TOOL;
     public static ItemInfinityDrill INFINITY_DRILL;
+    public static MobEssenceToolItem MOB_ESSENCE_TOOL;
 
     @Override
     public List<Feature.Builder> generateFeatures() {
@@ -26,6 +28,7 @@ public class ModuleTool implements IModule {
         features.add(Feature.builder("meat_feeder").content(Item.class, MEAT_FEEDER = new MeatFeederItem(TAB_TOOL)));
         features.add(Feature.builder("mob_imprisonment_tool").content(Item.class, MOB_IMPRISONMENT_TOOL = new MobImprisonmentToolItem(TAB_TOOL)));
         features.add(Feature.builder("infinity_drill").content(Item.class, INFINITY_DRILL = new ItemInfinityDrill(TAB_TOOL)));
+        features.add(Feature.builder("mob_essence_tool").content(Item.class, MOB_ESSENCE_TOOL = new MobEssenceToolItem(TAB_TOOL)));
         TAB_TOOL.addIconStack(new ItemStack(INFINITY_DRILL));
         return features;
     }
