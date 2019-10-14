c9ca7daaccb7501788f93f0128ec71df6320863c
Buuz135 <buuz135@hotmail.com>
2019-10-14 15:26:02 +0200

    Added missing recipes

--- src/main/java/com/buuz135/industrial/block/agriculturehusbandry/SlaughterFactoryBlock.java
+++ src/main/java/com/buuz135/industrial/block/agriculturehusbandry/SlaughterFactoryBlock.java
@@ -3,10 +3,17 @@ package com.buuz135.industrial.block.agriculturehusbandry;
 import com.buuz135.industrial.block.IndustrialBlock;
 import com.buuz135.industrial.block.agriculturehusbandry.tile.SlaughterFactoryTile;
 import com.buuz135.industrial.module.ModuleAgricultureHusbandry;
+import com.buuz135.industrial.utils.IndustrialTags;
 import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.recipe.generator.TitaniumShapedRecipeBuilder;
 import net.minecraft.block.Blocks;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
+import java.util.function.Consumer;
 
 public class SlaughterFactoryBlock extends IndustrialBlock<SlaughterFactoryTile> {
 
@@ -23,5 +30,18 @@ public class SlaughterFactoryBlock extends IndustrialBlock<SlaughterFactoryTile>
     @Override
     public RotationType getRotationType() {
         return RotationType.FOUR_WAY;
+    }
+
+    @Override
+    public void registerRecipe(Consumer<IFinishedRecipe> consumer) {
+        TitaniumShapedRecipeBuilder.shapedRecipe(this)
+                .patternLine("PDP").patternLine("SMS").patternLine("ARA")
+                .key('P', IndustrialTags.Items.PLASTIC)
+                .key('D', new ItemTags.Wrapper(new ResourceLocation("forge:gear/gold")))
+                .key('S', Items.IRON_SWORD)
+                .key('A', Items.IRON_AXE)
+                .key('M', IndustrialTags.Items.MACHINE_FRAME_PITY)
+                .key('R', Items.REDSTONE)
+                .build(consumer);
     }
 }
--- src/main/java/com/buuz135/industrial/block/core/DissolutionChamberBlock.java
+++ src/main/java/com/buuz135/industrial/block/core/DissolutionChamberBlock.java
@@ -3,10 +3,18 @@ package com.buuz135.industrial.block.core;
 import com.buuz135.industrial.block.IndustrialBlock;
 import com.buuz135.industrial.block.core.tile.DissolutionChamberTile;
 import com.buuz135.industrial.module.ModuleCore;
+import com.buuz135.industrial.utils.IndustrialTags;
 import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.recipe.generator.TitaniumShapedRecipeBuilder;
 import net.minecraft.block.Blocks;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 import javax.annotation.Nonnull;
+import java.util.function.Consumer;
 
 public class DissolutionChamberBlock extends IndustrialBlock<DissolutionChamberTile> {
 
@@ -23,5 +31,18 @@ public class DissolutionChamberBlock extends IndustrialBlock<DissolutionChamberT
     @Override
     public RotationType getRotationType() {
         return RotationType.FOUR_WAY;
+    }
+
+    @Override
+    public void registerRecipe(Consumer<IFinishedRecipe> consumer) {
+        TitaniumShapedRecipeBuilder.shapedRecipe(this)
+                .patternLine("PCP").patternLine("BMB").patternLine("GDG")
+                .key('P', IndustrialTags.Items.PLASTIC)
+                .key('C', Tags.Items.CHESTS)
+                .key('B', Items.BUCKET)
+                .key('M', IndustrialTags.Items.MACHINE_FRAME_PITY)
+                .key('G', Tags.Items.INGOTS_GOLD)
+                .key('D', new ItemTags.Wrapper(new ResourceLocation("forge:gear/diamond")))
+                .build(consumer);
     }
 }
