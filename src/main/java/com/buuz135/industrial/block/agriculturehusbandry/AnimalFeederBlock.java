15bc97fd7f719f174dc55962bef2e7c42f586b81
Buuz135 <buuz135@hotmail.com>
2019-10-15 13:40:52 +0200

    Added Animal Feeder recipe

--- src/main/java/com/buuz135/industrial/block/agriculturehusbandry/AnimalFeederBlock.java
+++ src/main/java/com/buuz135/industrial/block/agriculturehusbandry/AnimalFeederBlock.java
@@ -3,10 +3,18 @@ package com.buuz135.industrial.block.agriculturehusbandry;
 import com.buuz135.industrial.block.IndustrialBlock;
 import com.buuz135.industrial.block.agriculturehusbandry.tile.AnimalFeederTile;
 import com.buuz135.industrial.module.ModuleAgricultureHusbandry;
+import com.buuz135.industrial.utils.IndustrialTags;
 import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.recipe.generator.TitaniumShapedRecipeBuilder;
 import net.minecraft.block.Blocks;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 import javax.annotation.Nonnull;
+import java.util.function.Consumer;
 
 public class AnimalFeederBlock extends IndustrialBlock<AnimalFeederTile> {
 
@@ -23,5 +31,18 @@ public class AnimalFeederBlock extends IndustrialBlock<AnimalFeederTile> {
     @Override
     public RotationType getRotationType() {
         return RotationType.FOUR_WAY;
+    }
+
+    @Override
+    public void registerRecipe(Consumer<IFinishedRecipe> consumer) {
+        TitaniumShapedRecipeBuilder.shapedRecipe(this)
+                .patternLine("PAP").patternLine("CMC").patternLine("DGD")
+                .key('P', IndustrialTags.Items.PLASTIC)
+                .key('A', Items.GOLDEN_APPLE)
+                .key('C', Items.GOLDEN_CARROT)
+                .key('G', new ItemTags.Wrapper(new ResourceLocation("forge:gear/iron")))
+                .key('D', Tags.Items.DYES_PURPLE)
+                .key('M', IndustrialTags.Items.MACHINE_FRAME_PITY)
+                .build(consumer);
     }
 }
