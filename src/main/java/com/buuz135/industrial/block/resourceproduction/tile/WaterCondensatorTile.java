0af0102692a7ee1e221024bfd214751f1f91207c
Buuz135 <buuz135@hotmail.com>
2020-03-15 12:36:45 +0100

    Fixed Water Condensator, closes #711

--- src/main/java/com/buuz135/industrial/block/resourceproduction/tile/WaterCondensatorTile.java
+++ src/main/java/com/buuz135/industrial/block/resourceproduction/tile/WaterCondensatorTile.java
@@ -63,6 +63,6 @@ public class WaterCondensatorTile extends IndustrialWorkingTile<WaterCondensator
     @Nonnull
     @Override
     public WaterCondensatorTile getSelf() {
-        return null;
+        return this;
     }
 }
