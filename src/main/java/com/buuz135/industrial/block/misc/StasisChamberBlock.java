cb2caa83550a42180e3a0c3e8fb2e11f6648beba
Buuz135 <buuz135@hotmail.com>
2020-03-15 17:52:30 +0100

    Removed duplicate recipes and added recipes for stasis chamber

--- src/main/java/com/buuz135/industrial/block/misc/StasisChamberBlock.java
+++ src/main/java/com/buuz135/industrial/block/misc/StasisChamberBlock.java
@@ -3,10 +3,15 @@ package com.buuz135.industrial.block.misc;
 import com.buuz135.industrial.block.IndustrialBlock;
 import com.buuz135.industrial.block.misc.tile.StasisChamberTile;
 import com.buuz135.industrial.module.ModuleMisc;
+import com.buuz135.industrial.utils.IndustrialTags;
 import com.hrznstudio.titanium.api.IFactory;
+import com.hrznstudio.titanium.recipe.generator.TitaniumShapedRecipeBuilder;
 import net.minecraft.block.Blocks;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.Items;
 
 import javax.annotation.Nonnull;
+import java.util.function.Consumer;
 
 public class StasisChamberBlock extends IndustrialBlock<StasisChamberTile> {
 
@@ -23,5 +28,16 @@ public class StasisChamberBlock extends IndustrialBlock<StasisChamberTile> {
     @Override
     public RotationType getRotationType() {
         return RotationType.FOUR_WAY;
+    }
+
+    @Override
+    public void registerRecipe(Consumer<IFinishedRecipe> consumer) {
+        TitaniumShapedRecipeBuilder.shapedRecipe(this).patternLine("sss").patternLine("gmg").patternLine("ipi")
+                .key('s', Blocks.SOUL_SAND)
+                .key('g', Items.GHAST_TEAR)
+                .key('m', IndustrialTags.Items.MACHINE_FRAME_SIMPLE)
+                .key('i', IndustrialTags.Items.GEAR_GOLD)
+                .key('p', Blocks.PISTON)
+                .build(consumer);
     }
 }
