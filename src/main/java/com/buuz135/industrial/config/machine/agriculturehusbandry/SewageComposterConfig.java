b71429cd31a943434e5b32443730dc107eb3abeb
Buuz135 <buuz135@hotmail.com>
2020-05-05 18:06:13 +0200

    Fixed default energy consumption for Sewage Composter

--- src/main/java/com/buuz135/industrial/config/machine/agriculturehusbandry/SewageComposterConfig.java
+++ src/main/java/com/buuz135/industrial/config/machine/agriculturehusbandry/SewageComposterConfig.java
@@ -10,8 +10,8 @@ public class SewageComposterConfig {
     @ConfigVal(comment = "Cooldown Time in Ticks [20 Ticks per Second] - Default: [100 (5s)]")
     public static int maxProgress = 100;
 
-    @ConfigVal(comment = "Amount of Power Consumed per Tick - Default: [400FE]")
-    public static int powerPerTick = 400;
+    @ConfigVal(comment = "Amount of Power Consumed per Tick - Default: [40FE]")
+    public static int powerPerTick = 40;
 
     @ConfigVal(comment = "Max Stored Power [FE] - Default: [10000 FE]")
     public static int maxStoredPower = 10000;
