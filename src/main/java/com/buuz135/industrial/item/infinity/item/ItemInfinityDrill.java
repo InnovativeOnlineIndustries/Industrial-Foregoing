d1ae4e5f411f169733bdd063041b2c59f322cf97
Buuz135 <buuz135@hotmail.com>
2020-08-01 23:06:09 +0200

    Fixed Infinity drill not breaking dirt stuff

--- src/main/java/com/buuz135/industrial/item/infinity/item/ItemInfinityDrill.java
+++ src/main/java/com/buuz135/industrial/item/infinity/item/ItemInfinityDrill.java
@@ -62,12 +62,12 @@ import java.util.function.Consumer;
 
 public class ItemInfinityDrill extends ItemInfinity {
 
-    public static Material[] mineableMaterials = {Material.ANVIL, Material.CLAY, Material.GLASS, Material.ICE, Material.IRON, Material.PACKED_ICE, Material.PISTON, Material.ROCK, Material.SAND, Material.SNOW};
+    public static Material[] mineableMaterials = {Material.ORGANIC, Material.EARTH, Material.ANVIL, Material.CLAY, Material.GLASS, Material.ICE, Material.IRON, Material.PACKED_ICE, Material.PISTON, Material.ROCK, Material.SAND, Material.SNOW};
     public static int POWER_CONSUMPTION = 10000;
     public static int FUEL_CONSUMPTION = 30;
 
     public ItemInfinityDrill(ItemGroup group) {
-        super("infinity_drill", group, new Properties().maxStackSize(1).addToolType(ToolType.PICKAXE, 3).addToolType(ToolType.SHOVEL, 3), POWER_CONSUMPTION, FUEL_CONSUMPTION, false);
+        super("infinity_drill", group, new Properties().maxStackSize(1).addToolType(ToolType.PICKAXE, 6).addToolType(ToolType.SHOVEL, 6), POWER_CONSUMPTION, FUEL_CONSUMPTION, false);
     }
 
     @Override
