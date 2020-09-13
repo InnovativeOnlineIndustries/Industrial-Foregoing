1f567131a51547945da6e3b48314f0d6baeb15c7
Buuz135 <buuz135@hotmail.com>
2020-09-13 21:26:46 +0200

    Fixed compilation errors

--- src/main/java/com/buuz135/industrial/utils/data/IndustrialBlockstateProvider.java
+++ src/main/java/com/buuz135/industrial/utils/data/IndustrialBlockstateProvider.java
@@ -7,7 +7,11 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.generators.*;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.util.NonNullLazy;
 
 import java.util.List;
