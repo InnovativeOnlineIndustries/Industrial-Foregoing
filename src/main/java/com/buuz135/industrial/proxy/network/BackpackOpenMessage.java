2462c64c6862b9e531a471944368bf4d78f03c38
Buuz135 <buuz135@hotmail.com>
2021-01-09 17:39:02 +0100

    Make sure to check the existence of a proper backpack target, closes #946

--- src/main/java/com/buuz135/industrial/proxy/network/BackpackOpenMessage.java
+++ src/main/java/com/buuz135/industrial/proxy/network/BackpackOpenMessage.java
@@ -27,7 +27,6 @@ import com.buuz135.industrial.module.ModuleTool;
 import com.buuz135.industrial.worlddata.BackpackDataManager;
 import com.hrznstudio.titanium.network.Message;
 import com.hrznstudio.titanium.network.locator.LocatorFactory;
-import com.hrznstudio.titanium.network.locator.PlayerInventoryFinder;
 import com.hrznstudio.titanium.network.locator.instance.InventoryStackLocatorInstance;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -45,24 +44,24 @@ public class BackpackOpenMessage extends Message {
     protected void handleMessage(NetworkEvent.Context context) {
         context.enqueueWork(() -> {
             ServerPlayerEntity entity = context.getSender();
-                PlayerInventoryFinder.Target target = ItemInfinityBackpack.findFirstBackpack(Minecraft.getInstance().player);
-                ItemStack stack = target.getFinder().getStackGetter().apply(entity, target.getSlot());
-                if (stack.getItem() instanceof ItemInfinityBackpack) {
-                    if (!stack.hasTag() || !stack.getTag().contains("Id")){
-                        UUID id = UUID.randomUUID();
-                        CompoundNBT nbt = stack.getOrCreateTag();
-                        nbt.putString("Id", id.toString());
-                        BackpackDataManager.getData(entity.world).createBackPack(id);
-                        stack.setTag(nbt);
+                ItemInfinityBackpack.findFirstBackpack(Minecraft.getInstance().player).ifPresent(target -> {
+                    ItemStack stack = target.getFinder().getStackGetter().apply(entity, target.getSlot());
+                    if (stack.getItem() instanceof ItemInfinityBackpack) {
+                        if (!stack.hasTag() || !stack.getTag().contains("Id")){
+                            UUID id = UUID.randomUUID();
+                            CompoundNBT nbt = stack.getOrCreateTag();
+                            nbt.putString("Id", id.toString());
+                            BackpackDataManager.getData(entity.world).createBackPack(id);
+                            stack.setTag(nbt);
+                        }
+                        String id = stack.getTag().getString("Id");
+                        ItemInfinityBackpack.sync(entity.world, id, entity);
+                        IndustrialForegoing.NETWORK.get().sendTo(new BackpackOpenedMessage(target.getSlot(), target.getName()), entity.connection.getNetworkManager(), NetworkDirection.PLAY_TO_CLIENT);
+                        NetworkHooks.openGui(entity, ModuleTool.INFINITY_BACKPACK, buffer ->
+                                LocatorFactory.writePacketBuffer(buffer, new InventoryStackLocatorInstance(target.getName(), target.getSlot())));
+                        return;
                     }
-                    String id = stack.getTag().getString("Id");
-                    ItemInfinityBackpack.sync(entity.world, id, entity);
-                    IndustrialForegoing.NETWORK.get().sendTo(new BackpackOpenedMessage(target.getSlot(), target.getName()), entity.connection.getNetworkManager(), NetworkDirection.PLAY_TO_CLIENT);
-                    NetworkHooks.openGui(entity, ModuleTool.INFINITY_BACKPACK, buffer ->
-                            LocatorFactory.writePacketBuffer(buffer, new InventoryStackLocatorInstance(target.getName(), target.getSlot())));
-                    return;
-                }
-
+                });
         });
     }
 }
