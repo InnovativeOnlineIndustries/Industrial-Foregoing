45699840c3501a1d8677907a3a5bc02eda7c1623
Buuz135 <buuz135@hotmail.com>
2024-05-11 12:45:47 +0200

    Fixed Enchantment Applicator only checking for essence for tanks above it, closes #1245

--- src/main/java/com/buuz135/industrial/block/misc/tile/EnchantmentApplicatorTile.java
+++ src/main/java/com/buuz135/industrial/block/misc/tile/EnchantmentApplicatorTile.java
@@ -89,7 +89,13 @@ public class EnchantmentApplicatorTile extends IndustrialProcessingTile<Enchantm
         long amount = this.tank.getFluidAmount();
         TileEntity tileEntity = this.world.getTileEntity(this.pos.up());
         if (tileEntity != null && tileEntity.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).isPresent()){
-            amount += tileEntity.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).map(iFluidHandler -> iFluidHandler.drain(new FluidStack(ModuleCore.ESSENCE.getSourceFluid(), Integer.MAX_VALUE), IFluidHandler.FluidAction.SIMULATE).getAmount()).orElse(0);
+            amount += tileEntity.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).map(iFluidHandler -> {
+                FluidStack fluidStack = iFluidHandler.drain(Integer.MAX_VALUE, IFluidHandler.FluidAction.SIMULATE);
+                if (fluidStack.getFluid().isIn(IndustrialTags.Fluids.EXPERIENCE)) {
+                    return fluidStack.getAmount();
+                }
+                return 0;
+            }).orElse(0);
         }
         return !output.getLeft().isEmpty() && amount >= getEssenceConsumed(output.getRight()) && this.output.getStackInSlot(0).isEmpty();
     }
