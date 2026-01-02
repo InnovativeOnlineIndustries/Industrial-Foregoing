01d5a9f25563d1c412ae22cd0c076c7655ba05b4
flutz1 <fabianlutz98@gmail.com>
2026-01-02 23:35:00 +0100

    Handle special entity interactions and add owner-only capture config

---
+++ src/main/java/com/buuz135/industrial/config/ItemCoreConfig.java
@@ -0,0 +1,8 @@
+package com.buuz135.industrial.config;
+
+import com.hrznstudio.titanium.annotation.config.ConfigFile;
+import net.neoforged.fml.config.ModConfig;
+
+@ConfigFile(value = "item-core", type = ModConfig.Type.COMMON)
+public class ItemCoreConfig {
+}
