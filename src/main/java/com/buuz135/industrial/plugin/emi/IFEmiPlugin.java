6c7e8460716b89bbf6458812c8e0f876fc01fe03
Qther <qther@tuta.io>
2026-05-25 22:22:49 +0800

    emi sludge refiner

--- src/main/java/com/buuz135/industrial/plugin/emi/IFEmiPlugin.java
+++ src/main/java/com/buuz135/industrial/plugin/emi/IFEmiPlugin.java
@@ -24,6 +24,7 @@ import com.buuz135.industrial.plugin.emi.category.LatexProcessingUnitEmiCategory
 import com.buuz135.industrial.plugin.emi.category.MycelialGeneratorEmiCategory;
 import com.buuz135.industrial.plugin.emi.category.OreWasherEmiCategory;
 import com.buuz135.industrial.plugin.emi.category.SewageComposterEmiCategory;
+import com.buuz135.industrial.plugin.emi.category.SludgeRefinerEmiCategory;
 import com.buuz135.industrial.plugin.emi.category.StoneWorkEmiCategory;
 import com.buuz135.industrial.plugin.emi.category.StoneWorkGeneratorEmiCategory;
 import com.buuz135.industrial.plugin.emi.recipe.BioreactorEmiRecipe;
@@ -38,6 +39,7 @@ import com.buuz135.industrial.plugin.emi.recipe.LatexProcessingUnitEmiRecipe;
 import com.buuz135.industrial.plugin.emi.recipe.MycelialGeneratorEmiRecipe;
 import com.buuz135.industrial.plugin.emi.recipe.OreWasherEmiRecipe;
 import com.buuz135.industrial.plugin.emi.recipe.SewageComposterEmiRecipe;
+import com.buuz135.industrial.plugin.emi.recipe.SludgeRefinerEmiRecipe;
 import com.buuz135.industrial.plugin.emi.recipe.StoneWorkEmiRecipe;
 import com.buuz135.industrial.plugin.emi.recipe.StoneWorkGeneratorEmiRecipe;
 import com.buuz135.industrial.plugin.jei.category.BioReactorRecipeCategory;
@@ -91,6 +93,7 @@ public class IFEmiPlugin implements EmiPlugin {
     public static final DyeMixerEmiCategory DYE_MIXER_EMI_CATEGORY = new DyeMixerEmiCategory();
     public static final LatexProcessingUnitEmiCategory LATEX_PROCESSING_UNIT_EMI_CATEGORY = new LatexProcessingUnitEmiCategory();
     public static final SewageComposterEmiCategory SEWAGE_COMPOSTER_EMI_CATEGORY = new SewageComposterEmiCategory();
+    public static final SludgeRefinerEmiCategory SLUDGE_REFINER_EMI_CATEGORY = new SludgeRefinerEmiCategory();
 
     @Override
     public void initialize(EmiInitRegistry registry) {
@@ -204,5 +207,11 @@ public class IFEmiPlugin implements EmiPlugin {
         registry.addCategory(SEWAGE_COMPOSTER_EMI_CATEGORY);
         registry.addWorkstation(SEWAGE_COMPOSTER_EMI_CATEGORY, EmiStack.of(ModuleAgricultureHusbandry.SEWAGE_COMPOSTER));
         registry.addRecipe(new SewageComposterEmiRecipe());
+
+        registry.addCategory(SLUDGE_REFINER_EMI_CATEGORY);
+        registry.addWorkstation(SLUDGE_REFINER_EMI_CATEGORY, EmiStack.of(ModuleResourceProduction.SLUDGE_REFINER));
+        for (var output : SludgeRefinerEmiRecipe.getPossibleOutputs()) {
+            registry.addRecipe(new SludgeRefinerEmiRecipe(output));
+        }
     }
 }
--- src/main/java/com/buuz135/industrial/plugin/emi/recipe/SludgeRefinerEmiRecipe.java
+++ src/main/java/com/buuz135/industrial/plugin/emi/recipe/SludgeRefinerEmiRecipe.java
@@ -1,13 +1,15 @@
 package com.buuz135.industrial.plugin.emi.recipe;
 
-import com.buuz135.industrial.config.machine.agriculturehusbandry.SewageComposterConfig;
+import com.buuz135.industrial.config.machine.resourceproduction.SludgeRefinerConfig;
 import com.buuz135.industrial.module.ModuleCore;
 import com.buuz135.industrial.plugin.emi.IFEmiPlugin;
 import com.buuz135.industrial.plugin.emi.widget.EnergyBarEmiWidget;
 import com.buuz135.industrial.plugin.emi.widget.NormalTankEmiWidget;
+import com.buuz135.industrial.utils.IndustrialTags;
 import com.buuz135.industrial.utils.Reference;
 import com.hrznstudio.titanium.client.screen.addon.SlotsScreenAddon;
 import com.hrznstudio.titanium.client.screen.asset.DefaultAssetProvider;
+import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
 import net.minecraft.client.Minecraft;
@@ -19,12 +21,24 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.awt.*;
 import java.util.List;
 
-public class SewageComposterEmiRecipe extends CustomEmiRecipe {
-    public static final EmiStack SEWAGE = EmiStack.of(ModuleCore.SEWAGE.getSourceFluid().get(), 1000);
+public class SludgeRefinerEmiRecipe extends CustomEmiRecipe {
+    public static final EmiStack SLUDGE = EmiStack.of(ModuleCore.SLUDGE.getSourceFluid().get(), 500);
+    private static List<EmiStack> POSSIBLE_OUTPUTS = null;
 
-    public SewageComposterEmiRecipe() {
-        super(ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "/emi/sewage_composter"), IFEmiPlugin.SEWAGE_COMPOSTER_EMI_CATEGORY,
-                List.of(SEWAGE), List.of(EmiStack.of(ModuleCore.FERTILIZER.get())));
+    public SludgeRefinerEmiRecipe(EmiStack output) {
+        super(ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "/emi/sludge_refiner/" + output.getItemStack().getItem().builtInRegistryHolder().getKey().location().toString().replace(':', '/')), IFEmiPlugin.SLUDGE_REFINER_EMI_CATEGORY,
+                List.of(SLUDGE), List.of(output));
+    }
+
+    public static List<EmiStack> getPossibleOutputs() {
+        if (POSSIBLE_OUTPUTS == null) {
+            POSSIBLE_OUTPUTS = EmiIngredient.of(IndustrialTags.Items.SLUDGE_OUTPUT).getEmiStacks();
+            for (var output : POSSIBLE_OUTPUTS) {
+                output.setChance(1.0F / POSSIBLE_OUTPUTS.size());
+            }
+        }
+
+        return POSSIBLE_OUTPUTS;
     }
 
     @Override
@@ -39,12 +53,12 @@ public class SewageComposterEmiRecipe extends CustomEmiRecipe {
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
-        widgets.addFillingArrow(48, 19, SewageComposterConfig.maxProgress * 50);
+        widgets.addFillingArrow(48, 19, 100 * 50);
         widgets.addSlot(this.getOutputs().getFirst(), 79, 1).recipeContext(this);
 
-        int consumed = SewageComposterConfig.powerPerTick * SewageComposterConfig.maxProgress;
-        widgets.add(new EnergyBarEmiWidget(0, 0, consumed, Math.max(SewageComposterConfig.maxStoredPower, consumed)));
-        widgets.add(new NormalTankEmiWidget(SEWAGE, 1000, 24, 0));
+        int consumed = SludgeRefinerConfig.powerPerTick * 100;
+        widgets.add(new EnergyBarEmiWidget(0, 0, consumed, Math.max(SludgeRefinerConfig.maxStoredPower, consumed)));
+        widgets.add(new NormalTankEmiWidget(SLUDGE, SludgeRefinerConfig.maxSludgeTankSize, 24, 0));
 
         widgets.addDrawable(0, 0, 0, 0, (draw, mouseX, mouseY, delta) -> {
             SlotsScreenAddon.drawAsset(draw, Minecraft.getInstance().screen, DefaultAssetProvider.DEFAULT_PROVIDER, 80, 2, 0, 0, 3, integer -> Pair.of(18 * (integer % 1), 18 * (integer / 1)), integer -> ItemStack.EMPTY, true, integer -> new Color(DyeColor.ORANGE.getFireworkColor()), integer -> true, 1);
